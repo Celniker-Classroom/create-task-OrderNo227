f7c4f028e24abc0dded688984b937081d0a536ce
Order No. 227 <126615958+OrderNo227@users.noreply.github.com>
2026-04-14 20:53:32 +0000

    Began javascript by setting up some variables and functions with parameters. Will finish functions and create arrays next time

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // add javascript here
+let rawName = prompt("What is your name?");
+let username = rawName.charAt(0).toUpperCase() + rawName.slice(1).toLowerCase();
+document.getElementById("greeting").textContent = "Hi there, " + username + "!"; 
+let bodyPart = document.querySelector("input[type='radio']:checked");
+
+function showResult(bodyPart) {
+    if (bodyPart.id = "legs") {
+        
+    }
+}
+
+document.getElementById("result").addEventListener("click", showResult(bodyPart));
